32951d39d3686b7b7e4b80915e6cda9ab4293704
Ricardo Robles <rroblesgo@gmail.com>
2022-02-18 19:24:56 +0100

    Instalación de tailwind.css con npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,11 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="w-full">
+      <h1 className="text-4xl bg-red-300 font-bold w-5/6 mx-auto text-center rounded mt-5 py-3">
+        Hola Mundo!!
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
